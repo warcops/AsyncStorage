f18392a5dbe6e7987cdddf7ad75b6cb5d6dfcdc0
Kirill <seokirillstarsnab@gmail.com>
2016-10-25 14:29:34 +0300

    no message

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import AsyncStorageInterface from './interface/AsyncStorage';
 
 export default class AsyncStorage extends AsyncStorageInterface{
-    static setItem(key, value, callback){
+    setItem(key, value, callback){
         return new Promise(function (resolve) {
             localStorage[`@AsyncStorage:${key}`] = value;
             if(callback) callback();
             resolve();
         })
-    }
+    } 
 
-    static getItem(key, callback){
+    getItem(key, callback){
         return new Promise(function (resolve) {
             let value = localStorage[`@AsyncStorage:${key}`];
             let error;
